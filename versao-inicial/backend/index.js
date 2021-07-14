2f70d88ecb69754d57ee9db8a33f91ef5c76eb35
Arlan <arlan_marreiro@cal-com.com.br>
2021-07-14 18:38:03 -0400

    Configure Migration and Add Tables

--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -1,8 +1,16 @@
 const app = require('express')()
 const consign = require('consign')
 
+// Importando o DB para pegara conexão
+const db = require('./config/db')
+
+// Deixando disponivel - Já configurado : Fazer os Selects
+app.db = db
+
 consign()
     .then('./config/middlewares.js')
+    .then('/api')
+    .then('./config/routes.js')
     .into(app)
 
 app.listen(3000, () => {

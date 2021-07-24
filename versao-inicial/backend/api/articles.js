96fd166bde63f537b84c3af09ff2d565733c6ec2
Arlan <arlan_marreiro@cal-com.com.br>
2021-07-23 22:16:27 -0400

    Create Queries.js - Finishing Api Article 02

--- versao-inicial/backend/api/articles.js
+++ versao-inicial/backend/api/articles.js
@@ -1,4 +1,6 @@
 // nst { where } = require("../config/db")
+const queries = require('./queries')
+
 module.exports = app => {
     const { existsOrError } = app.api.validator
 
@@ -73,5 +75,22 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, remove, get, getById }
+    const getByCategory = async(req, res) => {
+        const categoryId = req.params.id
+        const page = req.query.page || 2
+        const categories = await app.db.row(queries.categoryWithChildren, categoryId)
+        const ids = categories.rows.map(c => c.id)
+
+        app.db({ a: 'articles', u: 'users' })
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+        limit(limit).offset(page * limit - limit)
+            .whereRaw('?? - ??', ['u.id', 'a.userId']) // Encontrar o usuario Autor do Artigo
+            .whereIn('categoryId', ids)
+            .orderBy('a.id', 'desc')
+            .then(articles => res.json(articles))
+            .catch(err => res.status(500).send(err))
+
+    }
+
+    return { save, remove, get, getById, getByCategory }
 }

8d14cfc349c4e85d4fe1c7fc65b3667d5f31c3bb
Arlan <arlan_marreiro@cal-com.com.br>
2021-08-05 20:25:07 -0400

    Validating Admin User

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin')
+
 module.exports = app => {
 
     // Autenticação - Url's Publicas
@@ -9,44 +11,45 @@ module.exports = app => {
 
     app.route('/users')
         .all(app.config.passport.authenticate())
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
 
     // Categorias
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
-        .get(app.api.category.get)
-        .post(app.api.category.save)
+        .get(admin(app.api.category.get))
+        .post(admin(app.api.category.save))
 
     // Cuidado com ordem! Tem que vir antes de: /categories/:id
     // Arvore de Array dos Menus
     app.route('/categories/tree')
         .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
-        // Categorias - Get - Put = Delete
+
+    // Categorias - Get - Put = Delete
     app.route('/categories/:id')
         .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
-        .put(app.api.category.save)
-        .delete(app.api.category.remove)
+        .put(admin(app.api.category.save))
+        .delete(admin(app.api.category.remove))
 
     // Rotas para os Artigos
     app.route('/articles')
         .all(app.config.passport.authenticate())
-        .get(app.api.articles.get)
-        .post(app.api.articles.save)
+        .get(admin(app.api.articles.get))
+        .post(admin(app.api.articles.save))
 
     app.route('/articles/:id')
         .all(app.config.passport.authenticate())
         .get(app.api.articles.getById)
-        .put(app.api.articles.save)
-        .delete(app.api.articles.remove)
+        .put(admin(app.api.articles.save))
+        .delete(admin(app.api.articles.remove)) //Adicionando  ADMIN na rota
 
     // Rotas Paginação Categorias
     app.route('/categories/:id/articles')

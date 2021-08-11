945d03b8d0074e8262b607403f4d42d2048a0c34
Arlan <arlan_marreiro@cal-com.com.br>
2021-08-10 20:18:32 -0400

    Validate Administrator Registration

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -11,9 +11,15 @@ module.exports = app => {
         return bcrypt.hashSync(password, salt)
     }
 
+
+
     const save = async(req, res) => {
         const user = {...req.body }
         if (req.params.id) user.id = req.params.id
+
+        // Validação para permitir que somente o Administrador possa cadastrar outro Adminitrador
+        if (!req.originalUrl.startWith('/users')) user.admin = false
+        if (!req.user || !req.user.admin) user.admin = true
 
         try {
             existsOrError(user.name, 'Nome não informado!')

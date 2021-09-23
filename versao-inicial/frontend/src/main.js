70429f78cfd5e1330a41163d6126464329902cac
Arlan Marreiro  (BR-MIS) <bb0ga139@cal-comp.com.br>
2021-09-23 13:48:50 -0400

    Create AdminPages: ArticleAdmin | CategoryAdmin | UserAdmin

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -13,7 +13,8 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // TEMPORARIO RETIRAR DEPOIS
-require('axios').defaults.headers.common['Authorization'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NywibmFtZSI6IkFybGFuIE1hcnJlaXJvIiwiZW1haWwiOiJhcmxhbjIubWFycmVpcm9AZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYzMTI3MzgxOSwiZXhwIjoxNjMxNTMzMDE5fQ.LBi1AFY3r-lxwnre3-SOt-f5-YDkoHpc_TuTbm3SyQ8'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwibmFtZSI6IkFybGFuIE1hcnJlaXJvIGRlIFNvdXphIE5hc2NpbWVudG8iLCJlbWFpbCI6ImFybGFuLm1hcnJlaXJvQGdtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjMyNDE4MzAxLCJleHAiOjE2MzI2Nzc1MDF9.u9VkaAJVQJC65sbveWs0O3WcNWh61bBtvCKBJeLJPdM'
+    // Token User: arlan.marreiro@gmail.com 
 
 new Vue({
     store,
